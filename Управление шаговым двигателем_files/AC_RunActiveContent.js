f82de1777ac3750a23b157577004f38c272ad587
bsn-robotics <sergiusbsn@yandex.ru>
2018-02-28 16:28:23 +0300

    Update 28.02.2018

---
+++ Управление шаговым двигателем_files/AC_RunActiveContent.js
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Страница не найдена</title>
+</head>
+
+<body>
+<html>
+<head>
+<title>Ошибка 404</title>
+</head>
+<body>
+<img src="http://electricalschool.info/404.jpg" alt="Ошибка 404">
+<p>К сожалению, запрашиваемая Вами страница не найдена.</p>
+<p>Почему?</p>
+<ol>
+<li>Ссылка, по которой Вы пришли, неверна.
+<li>Вы неправильно указали путь или название страницы.
+<li>Страница была удалёна со времени Вашего последнего посещения.
+</ol>
+<p>Для продолжения работы с сайтом Вы можете воспользоваться формой поиска по сайту:</p>
+<table border="0" width="100%">
+	<tr>
+		<td width="450"><div class="ya-site-form ya-site-form_inited_no" onclick="return {'bg': 'transparent', 'target': '_self', 'language': 'ru', 'suggest': true, 'tld': 'ru', 'site_suggest': true, 'action': 'http://electricalschool.info/sitesearch.html', 'webopt': false, 'fontsize': 14, 'arrow': false, 'fg': '#000000', 'searchid': '2064573', 'logo': 'rb', 'websearch': false, 'type': 3}"><form action="http://yandex.ru/sitesearch" method="get" target="_self"><input type="hidden" name="searchid" value="2064573" /><input type="hidden" name="l10n" value="ru" /><input type="hidden" name="reqenc" value="" />
+		<span lang="ru">&nbsp;</span><input type="text" name="text" value="" /><input type="submit" value="Найти" /></form></div><style type="text/css">.ya-page_js_yes .ya-site-form_inited_no { display: none; }</style><script type="text/javascript">(function(w,d,c){var s=d.createElement('script'),h=d.getElementsByTagName('script')[0],e=d.documentElement;(' '+e.className+' ').indexOf(' ya-page_js_yes ')===-1&&(e.className+=' ya-page_js_yes');s.type='text/javascript';s.async=true;s.charset='utf-8';s.src=(d.location.protocol==='https:'?'https:':'http:')+'//site.yandex.net/v2.0/js/all.js';h.parentNode.insertBefore(s,h);(w[c]||(w[c]=[])).push(function(){Ya.Site.Form.init()})})(window,document,'yandex_site_callbacks');</script>
+</td>
+		<td>&nbsp;</td>
+	</tr>
+</table>
+<p>..или перейти на:</p>
+<ul>
+<li><a href="http://electricalschool.info/">Главную страницу сайта</a></ul>
+</body>
+</html>
+</body>
+
+</html>
---
+++ Управление шаговым двигателем_files/extension_1086.js
@@ -0,0 +1,11 @@
+if (typeof adcm === 'object') {
+    adcm.callpixel(5246);
+    adcm.callpixel(6356);
+    adcm.callpixel(6350);
+    adcm.callpixel(6326);
+    adcm.callpixel(4002);
+    adcm.callpixel(4025);
+
+    adcm.callextpixel('//cm.p.altergeo.ru/pixel?url=https%3A%2F%2Fdmg.digitaltarget.ru%2F1%2F2016%2Fi%2Fi%3Fa%3D16%26e%3D%24%7BUSER_ID%7D%26c%3Dds%3A16.up%3A%24%7BUSER_ID%7D.rs%3A1086.pc%3A%24%7BCATS_ID%7D%26i%3D%24%7BRANDOM%7D');
+    adcm.callextpixel('//sync.omnidsp.com/match?src=amberdata');
+};
